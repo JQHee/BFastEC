c60d7e1efaf3219de7c7effda891320841f05f95
HJQ <v-huangjiaqian@midland.local>
2019-01-15 12:14:29 +0800

    框架导入

---
+++ latte_core/src/main/java/com/hjq/latte/activitys/ProxyActivity.java
@@ -0,0 +1,10 @@
+package com.hjq.latte.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+
+import me.yokeyword.fragmentation.ISupportActivity;
+
+public abstract class ProxyActivity extends AppCompatActivity implements ISupportActivity {
+
+
+}
---
+++ latte_core/src/main/java/com/hjq/latte/delegates/PermissionCheckerDelegate.java
@@ -0,0 +1,4 @@
+package com.hjq.latte.delegates;
+
+public class PermissionCheckerDelegate {
+}

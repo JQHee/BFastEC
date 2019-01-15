c668516b39282447e9fbae200d39065e7e012d81
HJQ <v-huangjiaqian@midland.local>
2019-01-15 14:37:47 +0800

    带完善单个Activity

--- latte_core/src/main/java/com/hjq/latte/delegates/LatteDelegate.java
+++ latte_core/src/main/java/com/hjq/latte/delegates/LatteDelegate.java
@@ -1,4 +1,9 @@
 package com.hjq.latte.delegates;
 
-public class LatteDelegate {
+public class LatteDelegate extends PermissionCheckerDelegate {
+
+//    @SuppressWarnings("unchecked")
+//    public <T extends LatteDelegate> T getParentDelegate() {
+//        return (T) getParentFragment();
+//    }
 }

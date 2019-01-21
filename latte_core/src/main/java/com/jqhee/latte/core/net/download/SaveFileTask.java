cefa7398383897db27c5046e56a1d48ce76ec288
HJQ <v-huangjiaqian@midland.local>
2019-01-21 14:45:18 +0800

    apk安装的调试

--- latte_core/src/main/java/com/jqhee/latte/core/net/download/SaveFileTask.java
+++ latte_core/src/main/java/com/jqhee/latte/core/net/download/SaveFileTask.java
@@ -164,18 +164,22 @@ public class SaveFileTask extends AsyncTask<Object, Integer, File> {
         }
         */
 
-        Intent intent = new Intent(Intent.ACTION_VIEW);
+
         if (FileUtil.getExtension(file.getPath()).equals("apk")) {
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            // 由于没有在Activity环境下启动Activity,设置下面的标签
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             //版本在7.0以上是不能直接通过uri访问的
             if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
-                // 由于没有在Activity环境下启动Activity,设置下面的标签
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                LatteLogger.d("版本大于 N ，开始使用 fileProvider 进行安装");
                 //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
-                Uri apkUri = FileProvider.getUriForFile(Latte.getApplication(), "com.example.chenfengyao.installapkdemo", file);
+                Uri apkUri = FileProvider.getUriForFile(Latte.getApplication(), "com.jqhee.bfastec.example", file);
                 //添加这一句表示对目标应用临时授权该Uri所代表的文件
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
             } else {
+                LatteLogger.d("正常安装");
                 intent.setDataAndType(Uri.fromFile(file),
                         "application/vnd.android.package-archive");
             }

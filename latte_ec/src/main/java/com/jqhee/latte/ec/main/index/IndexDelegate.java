b0500d19400383cce534c993e22cfe11f6caa501
HJQ <v-huangjiaqian@midland.local>
2019-01-25 15:47:18 +0800

    解决首页不能运行的问题

--- latte_ec/src/main/java/com/jqhee/latte/ec/main/index/IndexDelegate.java
+++ latte_ec/src/main/java/com/jqhee/latte/ec/main/index/IndexDelegate.java
@@ -2,12 +2,49 @@ package com.jqhee.latte.ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.joanzapata.iconify.widget.IconTextView;
 import com.jqhee.fast.ec.R;
+import com.jqhee.fast.ec.R2;
 import com.jqhee.latte.core.bottom.BottomItemDelegate;
 
+import butterknife.BindView;
+
 public class IndexDelegate extends BottomItemDelegate {
+
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout;
+    // @BindView(R2.id.icon_index_scan)
+    // IconTextView mIconScan;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView;
+    @BindView(R2.id.icon_index_message)
+    IconTextView mIconMessage;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar;
+
+
+    /**
+     * 刷新控件初始化
+     */
+    private void initRefreshLayout() {
+        // 设置颜色
+        mRefreshLayout.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        // 设置位置
+        mRefreshLayout.setProgressViewOffset(true, 120, 300);
+    }
+
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
@@ -16,5 +53,11 @@ public class IndexDelegate extends BottomItemDelegate {
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
 
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        initRefreshLayout();
     }
 }

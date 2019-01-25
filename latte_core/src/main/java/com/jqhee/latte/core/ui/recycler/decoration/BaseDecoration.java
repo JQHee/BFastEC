1821227665da328f2f5a4417b6f6c3ee62183ab8
HJQ <v-huangjiaqian@midland.local>
2019-01-25 16:46:23 +0800

    增加recycle自定义的分割线

---
+++ latte_core/src/main/java/com/jqhee/latte/core/ui/recycler/decoration/BaseDecoration.java
@@ -0,0 +1,18 @@
+package com.jqhee.latte.core.ui.recycler.decoration;
+
+import android.support.annotation.ColorInt;
+import com.choices.divider.DividerItemDecoration;
+
+/**
+ * recycle 的分割线
+ */
+public class BaseDecoration extends DividerItemDecoration {
+
+    private BaseDecoration(@ColorInt int color, int size) {
+        setDividerLookup(new DividerLookupImpl(color, size));
+    }
+
+    public static BaseDecoration create(@ColorInt int color, int size) {
+        return new BaseDecoration(color, size);
+    }
+}
